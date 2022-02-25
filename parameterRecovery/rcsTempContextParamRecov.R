35fdc7ebcb04021690cd9f5ae608ccfde7ff7476
hayley <hayleynroper@gmail.com>
2022-02-25 11:02:01 -0700

    Update rcsTempContextParamRecov.R

--- parameterRecovery/rcsTempContextParamRecov.R
+++ parameterRecovery/rcsTempContextParamRecov.R
@@ -5,7 +5,7 @@ rcsTempContextParRec <- function(n,subjdata){
   alloutput <- list() # Prepare the object into which we're going to put all the outputs of each iteration
   for(i in 1:n){ # n is iteration for each core
     lb = c(-2,.5,0.1); # poc, shift, earnings
-    ub = c(1, 4, 3);  # poc, shift, earnings
+    ub = c(.5, 4, 3);  # poc, shift, earnings
     initparval =  c(runif(1,-1.5,.5),runif(1,.5,3), runif(1,.1,2))
     alloutput[[i]] <- NA
     try({output = optim(initparval, tempContextLL, choiceset=subjdata, method= "L-BFGS-B", lower=lb, upper=ub,hessian=TRUE); #using tempContextLL function
