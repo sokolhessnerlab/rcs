1b3028777eea4ce882b9810c0b16ce77c9ef2d6b
hayley <hayleynroper@gmail.com>
2022-05-27 12:50:53 -0600

    add import details

--- task/rcsPrimary.py
+++ task/rcsPrimary.py
@@ -11,56 +11,59 @@ Primary script for HRB's dissertation project RCS
 This script does some set up for the experiment and calls all required scripts to run the risky decision-making and cognitive control tasks
 """
 
-# define the function and specify the arguments
-import os
+def rcsPrimary(subID)
 
-# import modules
-
-
-# configuration stuff?
-
-# set working directory
-os.chdir("/Users/hayley/Documents/GitHub/rcs/task")
-
-
-# set up monitor(s)
-
-
-# import scripts
-
-
-# read condition order from pre-existing text file
-
-
-# set up data structure, filename, etc
-
-
-# reminder of general instructions and practice trials
-
-
-# risky decision-making task round 1
-
-
-# anything we want to do before the second round that is not covered by RDM task
-    # save risky decision-making data (this will already be saved by the RDM task but perhaps we want to set it up so it can be easily combined with round 2)
-
-
-# risky decision-making task round 2
-
-
-# randomly select between the two outcomes in the risky decision-making tasks and display on screen and save that information
-
-
-# cognitive control (working memory) tasks
-
-
-# ospan instructions + instructions quiz + practice + task
-# symspan instructions + instructions quiz + practice + task
-
-# score WM and add to data? or maybe just add this to the analysis script 
-
-# save data
-
-
-
-
+    # define the function and specify the arguments
+    import os
+    
+    # import modules
+    from rcsRDMChoiceSet import *
+    from rcsRDM import *
+    
+    # configuration stuff?
+    
+    # set working directory
+    os.chdir("/Users/hayley/Documents/GitHub/rcs/task")
+    
+    
+    # set up monitor(s)
+    
+    
+    # import scripts
+    
+    
+    # read condition order from pre-existing text file
+    
+    
+    # set up data structure, filename, etc
+    
+    
+    # reminder of general instructions and practice trials
+    
+    
+    # risky decision-making task round 1
+    
+    
+    # anything we want to do before the second round that is not covered by RDM task
+        # save risky decision-making data (this will already be saved by the RDM task but perhaps we want to set it up so it can be easily combined with round 2)
+    
+    
+    # risky decision-making task round 2
+    
+    
+    # randomly select between the two outcomes in the risky decision-making tasks and display on screen and save that information
+    
+    
+    # cognitive control (working memory) tasks
+    
+    
+    # ospan instructions + instructions quiz + practice + task
+    # symspan instructions + instructions quiz + practice + task
+    
+    # score WM and add to data? or maybe just add this to the analysis script 
+    
+    # save data
+    
+    
+    
+    
