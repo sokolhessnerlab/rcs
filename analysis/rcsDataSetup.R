846a2239abda7e23a8a4651f06c72247b7cd6c61
hayley <hayleynroper@gmail.com>
2022-10-30 23:09:15 -0600

    clean up analysis scripts and run a few more models

--- analysis/rcsDataSetup.R
+++ analysis/rcsDataSetup.R
@@ -14,83 +14,18 @@ library(lme4)
 # load data frames that were saved in during QA(rcsDataQA.Rmd)
 load(file.path(config$path$data$Rdata,'rdmDF_clean.Rdata'))
 load(file.path(config$path$data$Rdata, 'RDMqualityCheck.Rdata'))
+load(file.path(config$path$data$Rdata, 'ospan_clean.Rdata'))
+load(file.path(config$path$data$Rdata, 'symspan_clean.Rdata'))
+load(file.path(config$path$data$Rdata, 'ERQscores.Rdata'))
+conditionsFile = read_csv('/Users/hayley/Documents/GitHub/rcs/task/rdmTask/rcsConditions.csv')
 
 
 # set up subject ID variables (using dataframe that has exclusion already applied)
 subIDchar = unique(rdmDFclean$subID)
 nSub = length(subIDchar)
-excludeSubID = c("012", "015")
+excludeSubID = c("012", "015", "022", "028")
 
 
-# PUTTING TOGETHER OTHER DATA FILES (OSPAN, SYMSPAN, RDM, POST-ROUND QUESTIONS AND POST-TASK QUESTIONS)
-# setting up file paths, loading individual sub files and pulling them together into one and apply exclusion
-
-# RDM post round
-rdmFilePath = (file.path(config$path$data$rdmData))
-rdmPostQFiles = list.files(rdmFilePath, pattern = "rcsPostQ_sub")
-
-#combine data into 1 file
-rdmPostRoundQsdf <- file.path(rdmFilePath,rdmPostQFiles) %>% 
-  lapply(read_csv) %>% 
-  bind_rows
-
-rdmPostRoundQsdf = rdmPostRoundQsdf[which(!rdmPostRoundQsdf$subID %in% excludeSubID),] # apply exclusion
-
-
-
-# Ospan
-ospanFilePath = (file.path(config$path$data$ospData))
-ospanFiles = list.files(ospanFilePath, pattern = "rcsOSPANbothReal_sub")
-
-ospansubid = regmatches(ospanFiles, regexpr("[0-9][0-9][0-9]", ospanFiles)) # pulls out subid from file names
-
-# combine data into 1 file
-ospanDF <- file.path(ospanFilePath,ospanFiles) %>% 
-  lapply(read_csv) %>% 
-  bind_rows
-
-ospanDF$subID = rep(ospansubid, each = 25); # add subID column
-
-
-ospanDF = ospanDF[which(!ospanDF$subID %in% excludeSubID),] # apply exclusion
-
-
-# Symspan
-symspanFilePath = (file.path(config$path$data$symspData))
-symspanFiles = list.files(symspanFilePath, pattern = "rcsSYMSPANbothReal_sub")
-
-symspansubid = regmatches(symspanFiles, regexpr("[0-9][0-9][0-9]", symspanFiles)) # pulls out subid from file names
-
-# combine data into one file
-symspanDF <- file.path(symspanFilePath,symspanFiles) %>% 
-  lapply(read_csv) %>% 
-  bind_rows
-
-symspanDF$subID = rep(symspansubid, each = 14); # add subID column
-
-
-symspanDF = symspanDF[which(!symspanDF$subID %in% excludeSubID),] # apply exclusion
-
-
-# ERQ and Demographic
-ERQdemoDF = read_csv(file.path(config$path$directory, 'data/RCS+ERQ+++Demographics_October+20,+2022_16.28.csv'))
-ERQdemoDF = ERQdemoDF[which(!ERQdemoDF$subID %in% excludeSubID),] # apply exclusion
-
-# Post-task questionnaires
-postTask = read_csv(file.path(config$path$directory, 'data/rcsPostTaskQuestionnaire.csv'))
-postTask = postTask[which(!postTask$subID %in% as.numeric(excludeSubID)),] # apply exclusion
-
-
-# removed missed trials:
-nanInd = which(is.na(rdmDFclean$choice));
-totNan = length(nanInd); # 84 missed trials 11/11
-subNan = unique(rdmDFclean$subID[nanInd]); #(11/11; 24 subs with missed trials)
-
-rdmDFclean = rdmDFclean[which(!is.nan(rdmDFclean$choice)),]; #remove missed trials
-
-
-# create variables for glmer
-rdmDFclean$evLevScaled = rdmDFclean$evLevel/max(rdmDFclean$riskyGain)
 
 # create a variable for condition code
 # 1 = control, control
@@ -98,37 +33,19 @@ rdmDFclean$evLevScaled = rdmDFclean$evLevel/max(rdmDFclean$riskyGain)
 # 3 = strategy, control
 # 4 = strategy, strategy
 
-conditionsFile = read_csv('/Users/hayley/Documents/GitHub/rcs/task/rdmTask/rcsConditions.csv')
 
-conditionsFile = conditionsFile[1:nSub,]
-sum(conditionsFile$condCode==1)
-sum(conditionsFile$condCode==2)
-sum(conditionsFile$condCode==3)
-sum(conditionsFile$condCode==4)
-
-condcode1 = conditionsFile$subID[conditionsFile$condCode==1]
-condcode2 = conditionsFile$subID[conditionsFile$condCode==2]
-condcode3 = conditionsFile$subID[conditionsFile$condCode==3]
-condcode4 = conditionsFile$subID[conditionsFile$condCode==4]
-
-# pgamble diff between round 1 and 2 
-# control, control
-mean(RDMqualityCheck$pgambleRound1[RDMqualityCheck$subID %in% as.numeric(condcode1)] -RDMqualityCheck$pgambleRound2[RDMqualityCheck$subID %in% as.numeric(condcode1)])
-
-# control, strategy
-mean(RDMqualityCheck$pgambleRound1[RDMqualityCheck$subID %in% as.numeric(condcode2)] -RDMqualityCheck$pgambleRound2[RDMqualityCheck$subID %in% as.numeric(condcode2)])
-
-# strategy, control
-mean(RDMqualityCheck$pgambleRound1[RDMqualityCheck$subID %in% as.numeric(condcode3)] -RDMqualityCheck$pgambleRound2[RDMqualityCheck$subID %in% as.numeric(condcode3)])
-
-# strategy, strategy
-mean(RDMqualityCheck$pgambleRound1[RDMqualityCheck$subID %in% as.numeric(condcode4)] -RDMqualityCheck$pgambleRound2[RDMqualityCheck$subID %in% as.numeric(condcode4)])
+conditionsFile = conditionsFile[!conditionsFile$subID %in% excludeSubID,] # this applies exclusion
+conditionsFile = conditionsFile[1:nSub,] # just include current participants up until this point
 
 
 
-scaleby = max(rdmDFclean$riskyGain, na.rm=T)
+# create variables for glmer
+scaleby = max(rdmDFclean$riskyGain, na.rm=T)# most variables will be scaled by this
+
+rdmDFclean$evLevScaled = rdmDFclean$evLevel/scaleby
+
+
 # Create a function that creates recent event variables for RCS dataset:
-
 rcs_past_event_variable <- function(DFname, DFwithVariable, trialsBack, DFwithSubID, DFwithRound, scaled){
   # DFname = name of the dataframe
   # DFwithVariable = full name of dataframe + variable name that we want to shift (e.g. rdmDFclean$outcome)
@@ -169,8 +86,6 @@ rcs_past_event_variable <- function(DFname, DFwithVariable, trialsBack, DFwithSu
 rdmDFclean$pastOC1 = rcs_past_event_variable(rdmDFclean,rdmDFclean$outcome, 1, as.numeric(rdmDFclean$subID),rdmDFclean$roundRDM, 0); # outcome t-1
 rdmDFclean$pastOC1sc = rdmDFclean$pastOC1/scaleby
 
-
-
 # create variables for shift analysis
 rdmDFclean$signedShift = c(0, diff(rdmDFclean$evLevel));
 rdmDFclean$signedShift[rdmDFclean$rdmTrial==1] = 0; # first trial is always 0
@@ -180,8 +95,7 @@ rdmDFclean$negShift = rdmDFclean$signedShift*as.numeric(rdmDFclean$signedShift<0
 
 # earnings rel. expectations.
 
-
-# Calculate cumulative earnings (within each phase) for each participant and scale trial so that it is 0-1 for each participant
+# Calculate cumulative earnings (within each round) for each participant and scale expectations so that it is 0-1 for each participant
 # earnings will be 0 to 1, normalized by each participant's max earnings
 # save the max cumulative earnings for each person in a vector
 
@@ -191,6 +105,7 @@ trialByRound = vector(); # to store scaled trial for each participant
 
 maxEarnSubRound= as.data.frame(matrix(data=NA, nrow = nSub, ncol = 3, dimnames=list(c(NULL), c("subID","maxEarnRound1", "maxEarnRound2"))));
 maxEarnSubRound$subID = 1:nSub;
+
 
 for (s in 1:nSub) {
   sub = rdmDFclean[rdmDFclean$subID==subIDchar[s],]
@@ -214,26 +129,26 @@ for (s in 1:nSub) {
 }
 
 
-rdmDFclean$earninngs = earningsByRound;
-rdmDFclean$earningsSC = earningsByRoundScaled;
+rdmDFclean$earnings = earningsByRound;
+rdmDFclean$earnNormalized01 = earningsByRoundScaled; # 0-1 (normalized within sub)
 rdmDFclean$trialSC = trialByRound;
-
+rdmDFclean$earnNormalizedOverall = rdmDFclean$earnings/max(rdmDFclean$earnings) # scale by max earnings overall
 
 # quick summary about cumulative earnings:
-# phase 1: range = $1576 - $2467, median = $2087, mean = $2080
-# phase 2: range = $1597 - $2492, median = $2135, mean = $2133
+# round 1: range = $1576 - $2467, median = $2096, mean = $2091
+# round 2: range = $1597 - $2492, median = $2103, mean = $2118
 
 
 
-# recode strategy
-rdmDFclean$strategy01 = rdmDFclean$strategy
-rdmDFclean$strategy01[rdmDFclean$strategy01==1] =-1
-rdmDFclean$strategy01[rdmDFclean$strategy01==2] =1
+# recode strategy (its currently 01, recode to be -1 and 1)
+rdmDFclean$strategyRecode = rdmDFclean$strategy
+rdmDFclean$strategyRecode[rdmDFclean$strategyRecode==0] =-1
+
 
 
 rdmDFclean$subIDnum = as.numeric(rdmDFclean$subID)
 
-
-rdmDFclean$round01 = rdmDFclean$roundRDM
-rdmDFclean$round01[rdmDFclean$round01 == 1] = 0
-rdmDFclean$round01[rdmDFclean$round01 == 2] = 1
+# recode round (current 1 and 2, recode to - and 1)
+rdmDFclean$roundRecode = rdmDFclean$roundRDM
+rdmDFclean$roundRecode[rdmDFclean$roundRecode == 1] = -1
+rdmDFclean$roundRecode[rdmDFclean$roundRecode == 2] = 1
